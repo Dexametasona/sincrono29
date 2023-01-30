29b4ef9c7b8a6708c353a244aa26af6d3edb887f
Dexametasona <machicadogomezalexander@gmail.com>
2023-01-30 08:57:41 -0500

    push para angular material

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'sincrono29',
+    appId: '1:529425031858:web:0f0a1cac86dc2669a55fab',
+    storageBucket: 'sincrono29.appspot.com',
+    apiKey: 'AIzaSyBEroSg1JiZVZS06FJ-gExq9aHGtGH21xw',
+    authDomain: 'sincrono29.firebaseapp.com',
+    messagingSenderId: '529425031858',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'sincrono29',
+    appId: '1:529425031858:web:0f0a1cac86dc2669a55fab',
+    storageBucket: 'sincrono29.appspot.com',
+    apiKey: 'AIzaSyBEroSg1JiZVZS06FJ-gExq9aHGtGH21xw',
+    authDomain: 'sincrono29.firebaseapp.com',
+    messagingSenderId: '529425031858',
+  },
   production: false
 };
 
